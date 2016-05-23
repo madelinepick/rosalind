82c9eee3aba0b4279219c1d98a758ac8514268a4
Madeline Pickering <madelinepickering@gmail.com>
2016-05-23 16:30:32 -0600

    db set up and migrations

---
+++ migrations/20160523153825_initial_tables.js
@@ -0,0 +1,52 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema
+  .createTable('users', function(table){
+  table.increments();
+  table.string('first_name');
+  table.string('last_name');
+  table.string('email');
+  })
+  .createTable('ancestry', function(table){
+  table.increments();
+  table.string('sub_saharan_african');
+  table.string('oceanian');
+  table.string('east_asian_native_american');
+  table.string('south_asian');
+  table.string('middle_eastern_north_african');
+  table.integer('user_id');
+  })
+  .createTable('snps', function(table){
+  table.increments();
+  table.string('rs7089424');
+  table.string('rs53576');
+  table.string('rs1800497');
+  table.string('rs17077540');
+  table.string('rs1121980');
+  table.string('rs2241880');
+  table.string('rs13266634');
+  table.string('rs2180439');
+  table.string('rs307377');
+  table.string('rs807701');
+  table.string('rs664143');
+  table.string('rs2802292');
+  table.string('rs1800955');
+  table.string('rs4307059');
+  table.string('rs10830963');
+  table.integer('user_id');
+  })
+  .createTable('snp_info', function(table){
+  table.increments();
+  table.string('name');
+  table.string('description');
+  table.string('advice_one');
+  table.string('advice_two');
+  })
+};
+exports.down = function(knex, Promise) {
+  return knex.schema
+  .dropTable('users')
+  .dropTable('ancestry')
+  .dropTable('snps')
+  .dropTable('snp_info')
+};
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var cookieParser = require('cookie-parser');
+var knex = require('knex')(require('../knexfile')['development']);
+var snps = 'rs4307059%20rs1800497%20rs53576%20rs10830963%20rs7089424%20rs10484554%20rs2241880%20rs2802292%20rs13266634%20rs2180439%20rs1121980%20rs664143%20rs307377%20rs1815739%20rs807701%20rs17077540%20rs1800955%20rs987525';
+
 
 /* GET home page. */
 router.get('/mental', function(req, res, next) {
@@ -15,35 +18,69 @@ router.get('/intentions', function(req, res, next) {
 });
 
 router.get('/', function(req, res, next) {
-  scope = 'rs41362547%20basic%20names%20ancestry';
+  scope = 'rs41362547%20basic%20names%20ancestry%20'+snps;
   if (req.signedCookies.access_token) {
-        var basic_info = {}, ancestry;
-        var base_uri = 'https://api.23andme.com/1';
-        var headers = {Authorization: 'Bearer ' + req.signedCookies.access_token};
-        request.get({ url: base_uri + '/demo/names/', headers: headers, json: true }, function (e, r, body) {
-            if(r.statusCode != 200) {
-                res.clearCookie('access_token');
-                res.redirect('/');
-            } else {
-                basic_info.first_name = body.first_name;
-                basic_info.last_name = body.last_name;
-                basic_info.profile_id = body.profiles[0].id;
-                request.get({ url: base_uri + '/demo/ancestry/'+basic_info.profile_id, headers: headers, json: true}, function (e, r, body) {
-                  if(r.statusCode != 200) {
-                      res.clearCookie('access_token');
-                      res.redirect('/');
-                  } else {
-                    ancestry = body.ancestry.sub_populations;
-                    request.get({ url: base_uri + '/demo/ancestry/'+basic_info.profile_id, headers: headers, json: true}, function (e, r, body) {
-                      res.render('ancestry', {
+    var basic_info = {}, ancestry, genotypes;
+    var base_uri = 'https://api.23andme.com/1';
+    var headers = {Authorization: 'Bearer ' + req.signedCookies.access_token};
+    request.get({ url: base_uri + '/demo/names/', headers: headers, json: true }, function (e, r, body) {
+      if(r.statusCode != 200) {
+        res.clearCookie('access_token');
+        res.redirect('/');
+      } else {
+          basic_info.first_name = body.first_name;
+          basic_info.last_name = body.last_name;
+          basic_info.profile_id = body.profiles[0].id;
+          request.get({ url: base_uri + '/demo/user/?email=true', headers: headers, json: true}, function (e, r, body) {
+            basic_info.email = body.email;
+            request.get({ url: base_uri + '/demo/ancestry/'+basic_info.profile_id, headers: headers, json: true}, function (e, r, body) {
+              ancestry = body.ancestry.sub_populations;
+              request.get({ url: base_uri + '/demo/genotypes/'+basic_info.profile_id+'/?locations='+snps+'&format=true', headers:  headers, json:true}, function (e, r, body) {
+              genotypes = body;
+              return knex('users').where({email:basic_info.email}).then(function(user){
+                if(user.length > 0){
+                  return knex('users')
+                      .innerJoin('ancestry', 'users.id', 'ancestry.user_id')
+                      .innerJoin('snps', 'users.id', 'genotypes.user_id')
+                      .then(function(results){
+                        console.log(results);
+                      })
+                } else {
+                  return knex('users').insert({email: basic_info.email, first_name: basic_info.first_name, last_name:basic_info.last_name})
+                        .returning('*')
+                        .then(function(user){
+                          console.log('genotypes in knex', genotypes.rs7089424);
+                          knex('snps').insert({
+                            rs7089424:genotypes.rs7089424,
+                            rs53576: genotypes.rs53576,
+                            rs1800497: genotypes.rs1800497,
+                            rs17077540: genotypes.rs17077540,
+                            rs1121980: genotypes.rs1121980,
+                            rs2241880: genotypes.rs2241880,
+                            rs13266634: genotypes.rs13266634,
+                            rs2180439: genotypes.rs2180439,
+                            rs307377: genotypes.rs307377,
+                            rs807701: genotypes.rs807701,
+                            rs664143: genotypes.rs664143,
+                            rs2802292: genotypes.rs2802292,
+                            rs1800955: genotypes.rs1800955,
+                            rs4307059: genotypes.rs4307059,
+                            rs10830963: genotypes.rs10830963,
+                            user_id: user.id
+                          })
+                      }).then(function(user){
+                        res.render('ancestry', {
                           ancestry: ancestry,
                           basic_info: basic_info
-                      });
-                    });
+                          });
+                      })
                   }
-                });
-            }
-        });
+                })
+              });
+            });
+          })
+        }
+      });
     } else {
       var pathway= 'https://api.23andme.com/authorize/?redirect_uri='+process.env.REDIRECT_URI+'&response_type=code&client_id='+ process.env.CLIENT_ID+'&scope='+scope;
       console.log(pathway);
@@ -57,7 +94,7 @@ router.get('/', function(req, res, next) {
     }
 });
 router.get('/receive_code/', function(req, res, next) {
-  scope = 'rs41362547%20basic%20names%20ancestry';
+  scope = 'rs41362547%20basic%20names%20ancestry%20'+snps;
   if (!req.query.code) {
       res.render('error', {
           client_id: process.env.CLIENT_ID,
